82b19b3d1ef35225adea8e1de8d36ce1ffd2db4f
kimsunkyung <skk095@naver.com>
2019-03-31 01:48:22 +0900

    mypage 추가

---
+++ Hanzip/app/src/main/java/com/example/user/hanzip/mypage/MypageActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.hanzip.mypage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.hanzip.R;
+
+public class MypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
